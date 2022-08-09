01cf7826c4de076d144d7719923f6f25acf992bb
Tal Hayut <talha@accessibe.com>
2022-08-09 19:17:01 +0300

    chore: init

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function IndexPage() {
   return (
     <div className="main">
-      <Link href="/birds">
-        <a>Birds Example</a>
-      </Link>
-      <Link href="/boxes">
-        <a>Boxes Example</a>
+      <Link href="/experience/experience">
+        <a>Experience</a>
       </Link>
     </div>
-  )
+  );
 }

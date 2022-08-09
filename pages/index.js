d42564dbb103583025f512c04a27c1f83037964d
Tal Hayut <talha@accessibe.com>
2022-08-09 19:23:25 +0300

    fix: no stupid index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
-import Link from 'next/link';
+import React, { Suspense, useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Environment } from '@react-three/drei';
+import Model from '../components/Model';
+import Overlay from '../components/Overlay';
 
 export default function IndexPage() {
+  const overlay = useRef();
+  const caption = useRef();
+  const scroll = useRef(0);
   return (
-    <div className="main">
-      <Link href="/experience">
-        <a>Experience</a>
-      </Link>
-    </div>
+    <>
+      <Canvas
+        shadows
+        onCreated={(state) => state.events.connect(overlay.current)}
+        raycaster={{ computeOffsets: ({ clientX, clientY }) => ({ offsetX: clientX, offsetY: clientY }) }}>
+        <ambientLight intensity={1} />
+        <Suspense fallback={null}>
+          <Model scroll={scroll} />
+          <Environment preset="city" />
+        </Suspense>
+      </Canvas>
+      <Overlay ref={overlay} caption={caption} scroll={scroll} />
+    </>
   );
 }

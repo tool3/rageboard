944dcd01167e90f1bcd0b01583cf261fa2c0a453
Tal Hayut <talha@accessibe.com>
2022-08-09 19:21:13 +0300

    fix: path

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function IndexPage() {
   return (
     <div className="main">
-      <Link href="/experience/experience">
+      <Link href="/experience">
         <a>Experience</a>
       </Link>
     </div>

1efb7e8a4c54255f6a575233a996fce199620883
Tal Hayut <talhayut.dev@gmail.com>
2023-06-11 13:15:27 +0300

    fix: no effects

--- pages/index.js
+++ pages/index.js
@@ -35,9 +35,9 @@ export default function IndexPage() {
         {/* <CameraShake yawFrequency={1} maxYaw={0.05} pitchFrequency={1} maxPitch={0.05} rollFrequency={0.5} maxRoll={0.5} intensity={0.2} /> */}
         <EffectComposer>
           {/* <Noise opacity={0.03} /> */}
-          <DepthOfField focusDistance={0.3} focalLength={1.8} bokehScale={5} height={880} blur={100} />
+          {/* <DepthOfField focusDistance={0.3} focalLength={1.8} bokehScale={5} height={880} blur={100} />
           <Bloom luminanceThreshold={0.5} luminanceSmoothing={0.9} height={300} />
-          <Vignette eskil offset={0} darkness={0.8} />
+          <Vignette eskil offset={0} darkness={0.8} /> */}
         </EffectComposer>
         <Stats />
       </Canvas>

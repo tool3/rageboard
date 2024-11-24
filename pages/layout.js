fb93b1706317890d417db004b7833f2346f16aff
Tal Hayut <tal.hayut@bigpanda.io>
2024-11-24 17:32:01 +0200

    fix: trying ga

--- pages/layout.js
+++ pages/layout.js
@@ -1,5 +1,5 @@
-import { GoogleTagManager } from '@next/third-parties/google';
 import Head from "next/head";
+import Script from 'next/script';
 
 
 export default function Layout({ children }) {
@@ -9,7 +9,14 @@ export default function Layout({ children }) {
             <meta name="description" content="rage keyboard made with threejs and blender" />
             <meta name="author" content="Tal Hayut" />
         </Head>
-        <GoogleTagManager gtmId={'G-50Y8D0TG3M'} />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-50Y8D0TG3M" />
+        <Script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+
+            gtag('config', 'G-50Y8D0TG3M');
+        </Script>
         <body>{children}</body>
     </html>
 }

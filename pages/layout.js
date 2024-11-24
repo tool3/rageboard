860adcf5ae29bf2c034660b0810e1555fd4564cf
Tal Hayut <tal.hayut@bigpanda.io>
2024-11-24 17:33:41 +0200

    fix: trying ga again

--- pages/layout.js
+++ pages/layout.js
@@ -12,7 +12,7 @@ export default function Layout({ children }) {
         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-50Y8D0TG3M" />
         <Script>
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments)}
+            function gtag(){window.dataLayer.push(arguments)}
             gtag('js', new Date());
 
             gtag('config', 'G-50Y8D0TG3M');

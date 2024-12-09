8a7b7bfd1ec8f669dc6b8ac2fbb40fa4e0256924
Tal Hayut <tal.hayut@bigpanda.io>
2024-12-09 22:31:54 +0200

    chore: og image vercel

--- pages/layout.js
+++ pages/layout.js
@@ -8,10 +8,6 @@ export default function Layout({ children }) {
             <title>Rage Board</title>
             <meta name="description" content="rage keyboard made with threejs and blender" />
             <meta name="author" content="Tal Hayut" />
-            <meta property="og:image" content="https://rageboard.vercel.app/images/rageboard-min.png" />
-            <meta property="og:image:type" content="image/png" />
-            <meta property="og:image:width" content="1920" />
-            <meta property="og:image:height" content="1080" />
             <meta property="og:description" content="RAGEBOARD" />
             <meta property="og:url" content="https://rageboard.vercel.app" />
             <meta property="og:type" content="website" />

aebb01ea99f86c8f0075c71056193f20354dea79
Tal Hayut <tal.hayut@bigpanda.io>
2024-12-09 21:09:47 +0200

    chore: adding og image

--- pages/layout.js
+++ pages/layout.js
@@ -8,6 +8,9 @@ export default function Layout({ children }) {
             <title>Rage Board</title>
             <meta name="description" content="rage keyboard made with threejs and blender" />
             <meta name="author" content="Tal Hayut" />
+            <meta property="og:image" content="/images/rageboard.png" />
+            <meta property="og:image:width" content="1920" />
+            <meta property="og:image:height" content="1080" />
         </Head>
         <GoogleAnalytics gaId={'G-50Y8D0TG3M'} />
         <body>{children}</body>

8b14c890865b0723bb82252a65d73f0c35afa78b
Tal Hayut <tal.hayut@bigpanda.io>
2024-12-09 21:19:23 +0200

    chore: adding og image

--- pages/layout.js
+++ pages/layout.js
@@ -13,6 +13,9 @@ export default function Layout({ children }) {
             <meta property="og:image:width" content="1920" />
             <meta property="og:image:height" content="1080" />
             <meta property="og:description" content="RAGEBOARD" />
+            <meta property="og:url" content="https://rageboard.vercel.app" />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content="RAGEBOARD" />
         </Head>
         <GoogleAnalytics gaId={'G-50Y8D0TG3M'} />
         <body>{children}</body>

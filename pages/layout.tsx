81738742dbe1e173df1776198edf81f815594110
Tal Hayut <tal.hayut@bigpanda.io>
2024-12-10 08:27:30 +0200

    chore: adding layout

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,76 +1,32 @@
 
-import React from 'react'
 import { GoogleAnalytics } from '@next/third-parties/google';
-import { NextSeo } from 'next-seo';
+import { Metadata } from 'next';
 
-const metadata = {
-    title: "RAGEBOARD",
-    description: "RAGEBOARD",
-    url: "https://rageboard.vercel.app",
-};
-
-
-const Header = () => {
-    return (
-        <NextSeo
-            themeColor='#FF6422'
-            title={metadata.title}
-            description={metadata.description}
-            canonical="https://rageboard.vercel.app"
-            twitter={{
-                site: "rageboard.vercel.app",
-                cardType: "summary_large_image"
-            }}
-            additionalMetaTags={[
-                {
-                    property: "keywords",
-                    content:
-                        "threejs, blender, 3d, glsl, react-three-fiber"
-                },
-                {
-                    name: "viewport",
-                    content: "width=device-width, initial-scale=1, maximum-scale=5"
-                },
-                {
-                    name: "apple-mobile-web-app-capable",
-                    content: "yes"
-                },
-                {
-                    name: "og:image:width",
-                    content: "1920"
-                },
-                {
-                    name: "og:image:height",
-                    content: "1080"
-                },
-                {
-                    name: "theme-color",
-                    content: "#FF6422"
-                }
-            ]}
-            openGraph={{
-                url: "https://rageboard.vercel.app",
-                title: metadata.title,
-                description: metadata.description,
-                images: [
-                    {
-                        url: "https://rageboard.vercel.app/images/rageboard_min.png",
-                        width: 1920,
-                        height: 1080,
-                        alt: "RAGEBOARD Logo",
-                        type: "image/png"
-                    }
-                ],
-                siteName: "ybh-law"
-            }}
-        />
-    )
+export const metadata: Metadata = {
+    title: {
+        default: 'rageboard-gallery | talhayut',
+        template: '%s | talhayut'
+    },
+    metadataBase: new URL('https://rageboard.vercel.app'),
+    description: `rageboard gallery`,
+    icons: [
+        {
+            rel: 'apple-touch-icon',
+            url: '/apple-touch-icon.png'
+        }
+    ],
+    manifest: '/manifest.webmanifest',
+    twitter: {
+        card: 'summary_large_image',
+        title: 'RAGEBOARD gallery',
+        creator: 'talhayut',
+        siteId: 'talhayut'
+    }
 }
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <Header />
             <GoogleAnalytics gaId={'G-50Y8D0TG3M'} />
 
             <body>{children}</body>

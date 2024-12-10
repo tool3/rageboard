e778a3652df1e20a985f2257874a00bbe2ff820d
Tal Hayut <tal.hayut@bigpanda.io>
2024-12-10 08:57:30 +0200

    chore: lower version nextseo

--- app/layout.js
+++ app/layout.js
@@ -1,42 +1,6 @@
 
 import { GoogleAnalytics } from '@next/third-parties/google';
 
-// export const metadata = {
-//     title: {
-//         default: 'rageboard | talhayut',
-//         template: '%s | talhayut'
-//     },
-//     metadataBase: new URL('https://rageboard.vercel.app'),
-//     description: `rageboard`,
-//     icons: [
-//         {
-//             rel: 'apple-touch-icon',
-//             url: '/apple-touch-icon.png'
-//         }
-//     ],
-//     openGraph: {
-//         url: "https://rageboard.vercel.app",
-//         title: metadata.title,
-//         description: metadata.description,
-//         images: [
-//             {
-//                 url: "https://rageboard.vercel.app/images/rageboard_min.png",
-//                 width: 1920,
-//                 height: 1080,
-//                 alt: "RAGEBOARD Logo",
-//                 type: "image/png"
-//             }
-//         ],
-//         siteName: "rageboard.vercel.app"
-//     },
-//     twitter: {
-//         card: 'summary_large_image',
-//         title: 'RAGEBOARD',
-//         creator: 'talhayut',
-//         siteId: 'talhayut'
-//     }
-// }
-
 export const metadata = {
     title: "RAGEBOARD",
     description: "rageboard 3d experience",
